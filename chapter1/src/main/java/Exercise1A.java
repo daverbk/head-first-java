c008b031a86710a7f81faf5140d2e295dec967bc
Dave Rabko <davidryabko13@gmail.com>
2023-10-23 21:01:05 +0300

    Add exercise1

---
+++ chapter1/src/main/java/Exercise1A.java
@@ -0,0 +1,16 @@
+// class declaration was missing
+public class Exercise1A {
+
+    public static void main(String[] args) {
+
+        int x = 5;
+
+        while (x > 1) {
+            x = x - 1;
+
+            if (x < 3) {
+                System.out.println("small x");
+            }
+        }
+    }
+}
